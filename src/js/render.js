935d4b688b66eda3ebc68b303e8915d62a531be5
wxzg <598449069@qq.com>
2021-08-10 09:48:25 +0800

    添加logo及部分UI功能

--- src/js/render.js
+++ src/js/render.js
@@ -2,10 +2,62 @@ const {ipcRenderer} = require('electron')
 const minWin = document.querySelector(".min-win")
 const closeWin = document.querySelector(".close-win")
 
+//缩小主界面
 minWin.onclick = () => {
     ipcRenderer.send('min-app')
 }
 
+//关闭主界面
 closeWin.onclick = () => {
     ipcRenderer.send('close-app')
+}
+
+
+//左侧导航栏按钮,右侧功能界面切换
+const task = document.querySelector('.task-page-wrap')
+const profile = document.querySelector('.profile-page-wrap')
+const proxy = document.querySelector('.proxy-page-wrap')
+const login = document.querySelector('.login-page-wrap')
+
+const task_btn = document.querySelector('.task-btn')
+const profile_btn = document.querySelector('.profile-btn')
+const proxy_btn = document.querySelector('.proxy-btn')
+const login_btn = document.querySelector('.login-btn')
+
+const title = document.querySelector('.title')
+
+//task界面
+task_btn.onclick = function(){
+    task.style.display = "flex"
+    profile.style.display = "none"
+    proxy.style.display = "none"
+    login.style.display = "none"
+    title.innerHTML = "Tasks"
+}
+
+//profile界面
+profile_btn.onclick = function(){
+    task.style.display = "none"
+    profile.style.display = "flex"
+    proxy.style.display = "none"
+    login.style.display = "none"
+    title.innerHTML = "Profile"
+}
+
+//proxy界面
+proxy_btn.onclick = function(){
+    task.style.display = "none"
+    profile.style.display = "none"
+    proxy.style.display = "flex"
+    login.style.display = "none"
+    title.innerHTML = "Proxy"
+}
+
+//用户login界面
+login_btn.onclick = function(){
+    task.style.display = "none"
+    profile.style.display = "none"
+    proxy.style.display = "none"
+    login.style.display = "flex"
+    title.innerHTML = "Login"
 }
